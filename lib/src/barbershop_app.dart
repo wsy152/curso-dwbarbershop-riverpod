8c6b34b9acf6666aa7a64b05fdee1d2ededeab0b
Edvaldo <35712346+wsy152@users.noreply.github.com>
2023-09-26 16:36:42 -0300

    configuração do asyncState

--- lib/src/barbershop_app.dart
+++ lib/src/barbershop_app.dart
@@ -1,19 +1,18 @@
+import 'package:asyncstate/asyncstate.dart';
 import 'package:curso_dw_barbershop/src/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class BarbershopApp extends StatelessWidget {
+  const BarbershopApp({super.key});
 
-  const BarbershopApp({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return MaterialApp(
+  @override
+  Widget build(BuildContext context) {
+    return AsyncStateBuilder(builder: (asyncNavigatorObserver) {
+      return MaterialApp(
+        navigatorObservers: [asyncNavigatorObserver],
         title: 'Curso DW Barber Shop',
-        routes:  {
-          '/' : (_) => const SplashPage()
-
-        },
-
-       );
+        routes: {'/': (_) => const SplashPage()},
+      );
+    });
   }
 }
